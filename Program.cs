de19bd72def8d15b35e9287b8f4036b0b82354f0
Luiz Filipe <luizfilipemn@hotmail.com>
2023-10-04 08:05:25 -0300

    projeto final estudo dotnet

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿void BoasVindas()
+﻿using System.ComponentModel;
+using System.Collections.Generic;
+
+void BoasVindas()
 {
     Console.WriteLine("\x1b[1;34m███╗░░░███╗░█████╗░██████╗░░██████╗░░██████╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░░██████╗");
         Console.WriteLine("\x1b[1;34m████╗░████║██╔══██╗██╔══██╗██╔═══██╗██╔════╝  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗██╔════╝");
@@ -31,9 +34,11 @@ void Menu()
         break;
         case 3:
         Console.WriteLine("Você selecionou a opção " + opcao + ".");
+        AvaliarBanda();
         break;
         case 4:
         Console.WriteLine("Você selecionou a opção " + opcao + ".");
+        ExibirMediaBanda();
         break;
         case 0:
         Console.WriteLine("Você decidiu sair");
@@ -44,7 +49,8 @@ void Menu()
     }
 }
 
-List<string>listaBanda = new List<string>(); //Listar bandas
+List<string>listaBandas = new List<string>();
+int quantidadeBandas = 0;
 
 void adicionarBanda()
 {
@@ -62,8 +68,8 @@ void adicionarBanda()
     genero = Console.ReadLine()!;
     Console.WriteLine($"A banda {nome} de gênero {genero} foi atualizada com sucesso!");
 
-    listaBanda.Add(nome);
-    // listaBanda.Add(genero);
+    listaBandas.Add(nome);
+    quantidadeBandas++;
 
     Console.WriteLine("Deseja cadastrar uma nova banda ou voltar para o menu principal?\nDigite 0 para voltar ao menu principal ou 1 para continuar no menu de cadastro de bandas.");
     opcoes = int.Parse(Console.ReadLine()!);
@@ -85,18 +91,95 @@ void adicionarBanda()
 void listarBandas()
 {
     Console.Clear();
-    for(int i =0; i < listaBanda.Count; i++){
-        Console.WriteLine($"Banda: {listaBanda[i]}");
+    foreach(string listaBanda in listaBandas){
+        Console.WriteLine($"Banda: {listaBanda}");
+    }
+    Console.WriteLine("\nDigite uma tecla para voltar ao menu principal.");
+    Console.ReadKey();
+    Menu();
+}
+
+Dictionary<string, List<double>> avaliacoesBanda = new Dictionary<string, List<double>>();
+
+
+void AvaliarBanda()
+{
+    Console.Clear();
+
+    if(quantidadeBandas == 0){
+        Console.WriteLine("Nenhuma banda cadastrada para avaliação.");
         Console.WriteLine("\nDigite uma tecla para voltar ao menu principal.");
         Console.ReadKey();
         Menu();
+        return;
     }
+
+    Console.WriteLine("Escolha a banda para avaliação:");
+    for(int i = 0; i < quantidadeBandas; i++){
+        Console.WriteLine($"{i + 1}. {listaBandas[i]}");
+    }
+
+    int escolha = int.Parse(Console.ReadLine()!);
+
+    if(escolha >= 1 && escolha <= quantidadeBandas){
+        string bandaEscolhida = listaBandas[escolha - 1];
+
+        Console.WriteLine($"Digite a avaliação para a banda {bandaEscolhida} (de 1 a 5):");
+        double avaliacao = double.Parse(Console.ReadLine()!);
+        if(avaliacoesBanda.ContainsKey(bandaEscolhida)){
+            avaliacoesBanda[bandaEscolhida].Add(avaliacao);
+        }else{
+            List<double> novaAvaliacao = new List<double> { avaliacao };
+            avaliacoesBanda.Add(bandaEscolhida, novaAvaliacao);
+        }
+        Console.WriteLine($"A banda {bandaEscolhida} foi avaliada com sucesso!");
+
+    }else{
+        Console.WriteLine("Opção inválida.");
+    }
+
+    Console.WriteLine("\nDigite uma tecla para voltar ao menu principal.");
+    Console.ReadKey();
+    Menu();
 }
 
-void adicionarMusica()
+void ExibirMediaBanda()
 {
+    Console.Clear();
+    if(quantidadeBandas == 0){
+        Console.WriteLine("Nenhuma banda cadastrada para avaliação.");
+        Console.WriteLine("\nDigite uma tecla para voltar ao menu principal.");
+        Console.ReadKey();
+        Menu();
+        return;
+    }
+
+    Console.WriteLine("Selecione a banda que você deseja visualizar a média: ");
+
+    for(int i = 0; i < quantidadeBandas; i++){
+        Console.WriteLine($"{i + 1}. {listaBandas[i]}");
+    }
+
+    int escolha = int.Parse(Console.ReadLine()!);
+
+    if(escolha >= 1 && escolha <= quantidadeBandas){
+        string bandaEscolhida = listaBandas[escolha - 1];
+        if(avaliacoesBanda.ContainsKey(bandaEscolhida)){
+            List<double> avaliacoes = avaliacoesBanda[bandaEscolhida];
+            double media = avaliacoes.Average();
+            Console.WriteLine($"A média de avaliação para a banda {bandaEscolhida} é: {media}");
+        }else{
+            Console.WriteLine($"A banda {bandaEscolhida} ainda não foi avaliada.");
+        }
+    }else{
+        Console.WriteLine("Opção inválida.");
+    }
+    Console.WriteLine("\nDigite uma tecla para voltar ao menu principal.");
+    Console.ReadKey();
+    Menu();
 
 }
+
 
 BoasVindas();
 Menu();
